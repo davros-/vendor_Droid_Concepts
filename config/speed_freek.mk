7c06c7c5361bf7239366d5d56ba852fe6b4140db
cphelps76 <cphelps76@gmail.com>
2013-06-09 14:08:52 -0500

    add init.d script to move and symlink dalvik-cache

--- config/speed_freek.mk
+++ config/speed_freek.mk
@@ -6,4 +6,5 @@ PRODUCT_COPY_FILES += \
     vendor/DEMENTED/prebuilt/demented/etc/init.d/02dementedkernel:system/etc/init.d/02dementedkernel \
     vendor/DEMENTED/prebuilt/demented/etc/init.d/05dementedfs:system/etc/init.d/05dementedfs \
     vendor/DEMENTED/prebuilt/demented/etc/init.d/91dementedzipalign:system/etc/init.d/91dementedzipalign \
-    vendor/DEMENTED/prebuilt/demented/etc/init.d/99dementedsystem:system/etc/init.d/99dementedsystem 
+    vendor/DEMENTED/prebuilt/demented/etc/init.d/99dementedsystem:system/etc/init.d/99dementedsystem \
+    vendor/DEMENTED/prebuilt/demented/etc/init.d/movecache:system/etc/init.d/movecache
