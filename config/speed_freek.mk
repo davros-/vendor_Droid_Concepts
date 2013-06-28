6133784dd5b4e79d02e6f181964edfa019ecf2ee
davros <davros@droidconcepts.com>
2013-06-28 03:00:45 -0400

    movecache script is no longer needed for the rom to work properly,
    and so goes away all the bullshit

--- config/speed_freek.mk
+++ config/speed_freek.mk
@@ -6,5 +6,5 @@ PRODUCT_COPY_FILES += \
     vendor/Droid_Concepts/prebuilt/Droid_Concepts/etc/init.d/02dementedkernel:system/etc/init.d/02dementedkernel \
     vendor/Droid_Concepts/prebuilt/Droid_Concepts/etc/init.d/05dementedfs:system/etc/init.d/05dementedfs \
     vendor/Droid_Concepts/prebuilt/Droid_Concepts/etc/init.d/91dementedzipalign:system/etc/init.d/91dementedzipalign \
-    vendor/Droid_Concepts/prebuilt/Droid_Concepts/etc/init.d/99dementedsystem:system/etc/init.d/99dementedsystem \
-    vendor/Droid_Concepts/prebuilt/Droid_Concepts/etc/init.d/movecache:system/etc/init.d/movecache
+    vendor/Droid_Concepts/prebuilt/Droid_Concepts/etc/init.d/99dementedsystem:system/etc/init.d/99dementedsystem 
+    
